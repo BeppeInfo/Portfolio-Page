da137e3ad4a5d140bd1bfeba8775237e5160f39f
Beppe Info <beppe.info@protonmail.com>
2026-09-02 22:37:53 -0300

    Fix missing current_path in 404 view

--- views/404.php
+++ views/404.php
@@ -4,6 +4,7 @@ echo render('layout', [
     'strings' => $strings,
     'lang' => $lang,
     'current_route' => $current_route,
+    'current_path' => '',
     'base_url' => $base_url,
     'content' => '<div class="page page-404"><h1>404</h1><p>' . e(t('common.not_found')) . '</p><a href="' . e(base_url()) . '/">' . e(t('common.go_home')) . '</a></div>',
 ]);
